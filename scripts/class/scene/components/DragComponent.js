babd954571eb37efada7d49e7978d9ce48de501c
lunan <radiochecker@gmail.com>
2015-04-08 22:33:38 +1200

    implement dragcomponent

--- scripts/class/scene/components/DragComponent.js
+++ scripts/class/scene/components/DragComponent.js
@@ -1,7 +1,8 @@
 define([
   'lib/easel',
-  'class/base/BaseComponent'
-  ], function(createjs,BaseComponent) {
+  'class/base/BaseComponent',
+  'class/service/SceneService'
+  ], function(createjs,BaseComponent,SceneService) {
   
   if (typeof createjs === 'undefined' ) {
       createjs = window.createjs;
@@ -23,13 +24,26 @@ define([
   
   p.initalize = function(obj){
       // no need to update the static object
+    var context = this;
+    this.dragging = false;
+    var scale = SceneService.getInstance().getCanvasScale();
     
+    obj.on("mousedown",function(evt) {
+			 context.detx = evt.stageX/scale - obj.get_x();
+			 context.dety = evt.stageY/scale - obj.get_y();
+			 context.dragging = true;
+		});
+		
     obj.on("pressmove",function(evt) {
-				// currentTarget will be the container that the event listener was added to:
-				evt.currentTarget.set_x(evt.stageX);
-				evt.currentTarget.set_y(evt.stageY);
- 
-			});
+			  
+				evt.currentTarget.set_x(evt.stageX/scale - context.detx);
+				evt.currentTarget.set_y(evt.stageY/scale - context.dety);
+		});
+		
+		obj.on("pressup",function(evt) {
+			 
+			 context.dragging = false;
+		});
   }
 
   createjs.DragComponent = createjs.promote(DragComponent, "BaseComponent");
